68b8af870507b831115457f3977f7a906a4af729
MuhammadZunair19 <lifedbs20@gmail.com>
2024-02-14 01:14:30 +0500

    Generic Data Fetching Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import GenreList from "./components/GenreList";
 import GameGrid from "./components/GameGrid";
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
 
       <Show above="lg">
         <GridItem
-          area="aside" >Aside
+          area="aside" >
+          <GenreList />
         </GridItem>
       </Show>
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useState,useEffect } from "react";
-import apiClient from '../services/api-client';
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Platform{
     id:number;
@@ -17,35 +15,8 @@ export interface Game {
 
 }
 
-interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-}
 
-const useGames=()=>{  
 
-    const [isLoading,setLoading]= useState(false);
+const useGames=()=> useData<Game>('/games');
 
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
-
-    useEffect(() => {
-        const controller=new AbortController();
-
-        setLoading(true);
-
-        apiClient.get<FetchGamesResponse>('./games',{signal:controller.signal})
-            .then((res) => {
-                setGames(res.data.results || [])
-                setLoading(false);
-            })
-            .catch(err => {
-                if(err instanceof CanceledError) return;
-                setError(err.message)
-                setLoading(false);
-            });
-            return()=>controller.abort();
-        }, []);
-    return {games,error,isLoading};
-}
 export default useGames;
