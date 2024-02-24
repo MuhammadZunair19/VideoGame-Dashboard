96a8b1cd8b0a2b4452d32bdca44e08ce10037aac
MuhammadZunair19 <lifedbs20@gmail.com>
2024-02-24 21:12:18 +0500

    building platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GenreList from "./components/GenreList";
 import GameGrid from "./components/GameGrid";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
   const [selectGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -28,11 +29,12 @@ function App() {
       <Show above="lg">
         <GridItem
           area="aside" paddingX={5}>
-          <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
+          <GenreList selectedGenre={selectGenre} onSelectGenre={(genre) => setSelectedGenre(genre)} />
         </GridItem>
       </Show>
 
       <GridItem area="main" >
+        <PlatformSelector />
         <GameGrid selectedGenre={selectGenre} />
       </GridItem>
     </Grid>
